839380a51f311d82d9dadcfe61862eb83813770f
itaintkevin <kpjtofficial@gmail.com>
2023-09-11 19:44:42 +0530

    feat: File Upload Set up

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,51 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import { WebBundlr } from "@bundlr-network/client"
+import { AppContext } from '@/context/AppContext'
+import { useState, useRef } from 'react'
+import { providers, utils } from 'ethers'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  const [bundlrInstance, setBundlrInstance] = useState()
+  const [balance, setBalance] = useState()
+  const bundlrRef = useRef()
+  async function initialiseBundlr() {
+    await window.ethereum.enable()
+  
+    const provider = new providers.Web3Provider(window.ethereum);
+    await provider._ready()
+  
+    const bundlr = new WebBundlr("https://node1.bundlr.network", "matic", provider)
+    await bundlr.ready()
+    
+    setBundlrInstance(bundlr)
+    bundlrRef.current = bundlr
+    fetchBalance()
+  }
+
+  async function fetchBalance() {
+    const bal = await bundlrRef.current.getLoadedBalance()
+    console.log('bal: ', utils.formatEther(bal.toString()))
+    setBalance(utils.formatEther(bal.toString()))
+  }
+
+  return (
+    <div style={containerStyle}>
+      <AppContext.Provider value={{
+        initialiseBundlr,
+        bundlrInstance,
+        balance,
+        fetchBalance
+      }}>
+        <Component {...pageProps} />
+      </AppContext.Provider>
+    </div>
+  )
 }
+
+const containerStyle = {
+  width: '900px',
+  margin: '0 auto',
+  padding: '40px'
+}
+
+export default MyApp
